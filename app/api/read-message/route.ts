3faa625b4f17cccaf13e4b5ded2c59a5980454d3
Olanetsoft <heedris2olubisi@gmail.com>
2024-05-28 21:51:49 +0100

    fix: use ethers

--- app/api/read-message/route.ts
+++ app/api/read-message/route.ts
@@ -10,36 +10,36 @@ const OPTIMISM_RPC_URL = "https://optimism-rpc.publicnode.com";
 const OPTIMISM_CONTRACT_ADDRESS = "0x010e4B8eb87991cD9De316dD614023D1a368b28d";
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
-  //   const provider = new ethers.JsonRpcProvider(OPTIMISM_RPC_URL);
-  //   const contract = new ethers.Contract(
-  //     OPTIMISM_CONTRACT_ADDRESS,
-  //     SendMessageABI,
-  //     provider
-  //   );
+  const provider = new ethers.JsonRpcProvider(OPTIMISM_RPC_URL);
+  const contract = new ethers.Contract(
+    OPTIMISM_CONTRACT_ADDRESS,
+    SendMessageABI,
+    provider
+  );
 
   try {
-    //     const value = await contract.value(); // Assuming `value` is the function you want to call
+    const value = await contract.value(); // Assuming `value` is the function you want to call
 
-    //     return NextResponse.json({ value });
+    return NextResponse.json({ value });
 
-    const data = encodeFunctionData({
-      abi: SendMessageABI,
-      functionName: "value",
-      // args: ["optimism", OPTIMISM_CONTRACT_ADDRESS, body.untrustedData.inputText],
-    });
+    // const data = encodeFunctionData({
+    //   abi: SendMessageABI,
+    //   functionName: "value",
+    //   // args: ["optimism", OPTIMISM_CONTRACT_ADDRESS, body.untrustedData.inputText],
+    // });
 
-    const txData: FrameTransactionResponse = {
-      chainId: `eip155:${optimism.id}`,
-      method: "eth_sendTransaction",
-      params: {
-        abi: [],
-        data,
-        to: OPTIMISM_CONTRACT_ADDRESS,
-        value: parseEther("0").toString(),
-        //   value: gas.toString(),
-      },
-    };
-    return NextResponse.json(txData);
+    // const txData: FrameTransactionResponse = {
+    //   chainId: `eip155:${optimism.id}`,
+    //   method: "eth_sendTransaction",
+    //   params: {
+    //     abi: [],
+    //     data,
+    //     to: OPTIMISM_CONTRACT_ADDRESS,
+    //     value: parseEther("0").toString(),
+    //     //   value: gas.toString(),
+    //   },
+    // };
+    // return NextResponse.json(txData);
   } catch (error) {
     return new NextResponse(`Error reading contract: ${error}`, {
       status: 500,
