165b7935a9158ce1e6cb516f9b4df968044770ee
Olanetsoft <heedris2olubisi@gmail.com>
2024-05-28 16:47:02 +0100

    fix: update to optimism mainnet

--- app/api/send-message/route.ts
+++ app/api/send-message/route.ts
@@ -5,8 +5,10 @@ import { base } from "viem/chains";
 import SendMessageABI from "../../_contracts/SendMessageABI";
 import type { FrameTransactionResponse } from "@coinbase/onchainkit/frame";
 
-const FANTOM_CONTRACT_ADDRESS = "0xBD8B9fA2BC1Fd65c8bb51132de5026F6E2a29C2E";
-const CELO_CONTRACT_ADDRESS = "0x4E703bd524eaA03F3685026a2428e3fDF258Da37";
+// const FANTOM_CONTRACT_ADDRESS = "0xBD8B9fA2BC1Fd65c8bb51132de5026F6E2a29C2E";
+// const CELO_CONTRACT_ADDRESS = "0x4E703bd524eaA03F3685026a2428e3fDF258Da37";
+const BASE_CONTRACT_ADDRESS = "0x5768bE56b4a3Bb3e62C464008e280226eb758fCF";
+const OPTIMISM_CONTRACT_ADDRESS = "0x010e4B8eb87991cD9De316dD614023D1a368b28d";
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   const body: FrameRequest = await req.json();
@@ -20,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   const data = encodeFunctionData({
     abi: SendMessageABI,
     functionName: "setRemoteValue",
-    args: ["celo", CELO_CONTRACT_ADDRESS, body.untrustedData.inputText],
+    args: ["optimism", OPTIMISM_CONTRACT_ADDRESS, body.untrustedData.inputText],
   });
 
   const txData: FrameTransactionResponse = {
@@ -29,7 +31,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
     params: {
       abi: [],
       data,
-      to: FANTOM_CONTRACT_ADDRESS,
+      to: BASE_CONTRACT_ADDRESS,
       value: parseEther("0.00004").toString(), // 0.00004 ETH
     },
   };
